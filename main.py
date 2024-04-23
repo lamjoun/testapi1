37023e0c4b0d7b27736601ee7d8dcaa039919904
Rachid LAMJOUN <93473696+lamjoun@users.noreply.github.com>
2024-04-23 15:13:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-import os
-from flask import Flask
+import uvicorn
+from fastapi import FastAPI
 
-app = Flask(__name__)
+V = 100
+app = FastAPI()
 
-@app.route('/')
-def hello_world():
-    target = os.environ.get('TARGET', 'World')
-    return 'Hello*-*-**** {}!\n'.format(target)
+@app.get("/")
+async def root():
+    return {"message": "===Hello World==="}
+
+@app.get("/valeurv")
+async def valeur_v():
+    return {"message": f"La variable V est : {V}"}
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8081)))
