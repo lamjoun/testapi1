b0654d99a2ca8cc58f50d3dbcbe150f63fc63c66
Rachid LAMJOUN <93473696+lamjoun@users.noreply.github.com>
2024-04-23 15:26:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ async def root():
     return {"message": "===Hello World==="}
 
 @app.get("/valeurv")
-async def valeur_v():
+async def valeurv():
     return {"message": f"La variable V est : {V}"}
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8081)))
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
