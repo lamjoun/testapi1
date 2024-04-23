90240ca5c41a84043bbd9dd82c0003346f2a8c45
Rachid LAMJOUN <93473696+lamjoun@users.noreply.github.com>
2024-04-23 12:59:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
+from fastapi.param_functions import Depends
 
+V = 100
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "===Hello World==="}
+
+@app.get("/valeur_v")
+async def valeur_v():
+    return {"message": f"La variable V est : {V}"}
